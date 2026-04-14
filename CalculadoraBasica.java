cd2891b69a2846c490dda42267044bae9f1b6fab
Rafamendoza11 <rafa50932@gmail.com>
2026-04-14 13:43:37 +0200

    feat: Sumayresta

---
+++ CalculadoraBasica.java
@@ -0,0 +1,12 @@
+package CalculadoraJava;
+public class CalculadoraBasica {
+
+    public int suma(int a, int b) {
+        return a + b;
+    }
+
+    public int resta(int a, int b) {
+        return a - b;
+    }
+}
+    
