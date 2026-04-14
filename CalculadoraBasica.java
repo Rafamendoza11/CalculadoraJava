20975eca7a94f93903c5f7d88cf207f1168824b0
Rafamendoza11 <rafa50932@gmail.com>
2026-04-14 13:54:46 +0200

    feat: suma u resta

--- CalculadoraBasica.java
+++ CalculadoraBasica.java
@@ -8,5 +8,11 @@ public class CalculadoraBasica {
     public int resta(int a, int b) {
         return a - b;
     }
+
+    public static void main(String[] args) {
+        CalculadoraBasica calc = new CalculadoraBasica();
+
+        System.out.println("Suma: " + calc.suma(10, 5));
+        System.out.println("Resta: " + calc.resta(10, 5));
+    }
 }
-    
